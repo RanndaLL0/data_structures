01c48852013240301346c25aed557873bef81e91
RanndaLL0 <felipevictor5120@gmail.com>
2026-03-11 22:08:25 -0300

    feat: implementing new methods for binary search tree

--- dictionary/dictionary.c
+++ dictionary/dictionary.c
@@ -7,7 +7,7 @@
 struct Node *create_node_bst(void *data, unsigned long data_size);
 void destroy_node(struct Node *node_to_destroy);
 struct Node *iterate_bst(struct binary_search_tree *binary_search_tree,
-                         struct Node *element, int *direction);
+                         struct Node *element,void *data, int *direction);
 void recursive_tree_destruction(struct Node *node_to_destroy);
 
 void *search_bst(struct binary_search_tree *binary_search_tree, void *data);
@@ -32,11 +32,49 @@ void binary_search_tree_destructor(
 
 struct Node *create_node_bst(void *data, unsigned long data_size) {
 
-  struct Node new_node = (struct Node *)malloc(sizeof(Node));
+  struct Node *new_node = (struct Node *)malloc(sizeof(struct Node));
   *new_node = node_constructor(data, data_size);
   return new_node;
 }
 
 void destroy_node_bst(struct Node *node_to_destroy) {
   node_destructor(node_to_destroy);
+}
+
+struct Node *iterate_bst(struct binary_search_tree *tree, struct Node *element, void *data, int *direction) {
+
+        if (tree->compare(element->data, data) == 1) {
+                
+                if (element->next) {
+                        return iterate_bst(tree, element->next, data, direction);
+                }
+                else {
+                        *direction = 1;
+                        return element;
+                }
+        }
+        else if(tree->compare(element->data, data) == -1){
+                
+                if (element->previous) {
+                        return iterate_bst(tree,element->previous, data,direction);
+                }
+                else {
+                        *direction = -1;
+                        return element;
+                }
+        }
+        else {
+                *direction = 0;
+                return element;
+        }
+}
+
+void recursive_tree_destruction(struct Node *node) {
+        if (node->previous) {
+                recursive_tree_destruction(node->previous);
+        }
+        else {
+                recursive_tree_destruction(node->next);
+        }
+        destroy_node(node);
 }
